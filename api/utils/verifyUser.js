03f2cfbd043c5150eefd4dc140a2634f48ca7eb5
Muhammad Rehman <muhammadrehmanmusharafali@gmail.com>
2024-06-20 07:14:57 +0500

    dashboard height change

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,9 +2,9 @@ import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 import usermodel from "../models/user.model.js";
 export const verifyToken = (req, res, next) => {
-  console.log("Verify Token Function Run!", req.cookies.access_token);
+  // console.log("Verify Token Function Run!", req.cookies.access_token);
   const token = req.cookies.access_token;
-  console.log("token is: ", token);
+  // console.log("token is: ", token);
   if (!token) return next(errorHandler(401, "Unauthorized"));
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, "Forbidden"));
@@ -13,9 +13,9 @@ export const verifyToken = (req, res, next) => {
   });
 };
 export const verifyadminToken = (req, res, next) => {
-  console.log("Verify Token by admin Function Run!", req.cookies);
+  // console.log("Verify Token by admin Function Run!", req.cookies);
   const token = req.cookies.access_token;
-  console.log("token is: ", token);
+  // console.log("token is: ", token);
   if (!token) return next(errorHandler(401, "Unauthorized"));
   jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
     user = await usermodel.findById(user.id);
--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -178,6 +178,7 @@ const AdminDashboard = () => {
             <section className="w-full lg:w-4/5">
               <div
                 id="main"
+                style={{ minHeight: "100vh" }}
                 className="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5"
               >
                 <div className="bg-gray-800 pt-3">
