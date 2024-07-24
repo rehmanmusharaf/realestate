3ddc9d5cc574f573ceebd1810e71a22066f00294
Muhammad Rehman <124819760+rehmanmusharaf@users.noreply.github.com>
2024-07-24 23:37:02 +0500

    Update index.js for Cors Setting

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 // Enable CORS with credentials
 app.use(
   cors({
-    origin: "http://localhost:5173", // replace with your frontend URL
+    origin: "https://gilded-sprite-b80903.netlify.app", // replace with your frontend URL
     credentials: true,
   })
 );
